f188d9fc443209f2404183c4f3f198cb5d63bb1a
Maxim Dzyuba <dev@prodengi.kz>
2018-05-13 00:10:23 +0600

    add roator8

--- app/js/script.js
+++ app/js/script.js
@@ -7,9 +7,15 @@ $(function(){
         autoplay: true,
         autoplaySpeed: 10000
     });
-    $('.js-slider-route, .js-slider-how, .js-slider-rules, .js-slider-video').slick({
+    $('.js-slider-route, .js-slider-how, .js-slider-rules').slick({
         arrows: false,
         dots: true,
+        fade: true,
+        speed: 500
+    });
+    $('.js-slider-video').slick({
+        arrows: false,
+        dots: false,
         fade: true,
         speed: 500
     });
@@ -26,10 +32,6 @@ $(function(){
       $('.' + parent).slick('slickGoTo', parseInt(slide));
       if (parent == 'js-main-slider') {
         $('.js-toggle-slide').text(parseInt(slide) + 1);
-      }
-      if (parent == 'js-slider-video') {
-        $('.youtube-section').hide();
-        $('.youtube-section[data-section="' + slide + '"]').show();
       }
     });
     $('.js-show-block').on('click tap', function(){
